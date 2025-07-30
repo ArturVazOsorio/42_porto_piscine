9b7370168587335a6f182a9bbf030a52489611fc
Nikhain <arturvitorantelavazosorio@gmail.com>
2025-07-30 05:03:55 +0100

    testando outra forma de impressao

---
+++ 02C00/ex05/ft_print_combteste.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nikhain <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 05:32:37 by nikhain           #+#    #+#             */
+/*   Updated: 2025/07/30 05:02:04 by nikhain          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(void)
+{
+	char	pos1;
+	char	pos2;
+	char	pos3;
+
+	pos1 = '0';
+	while (pos1 <= '7')
+	{
+		pos2 = pos1 + 1;
+		while (pos2 <= '8')
+		{
+			pos3 = pos2 + 1;
+			while (pos3 <= '9')
+			{
+				ft_putchar(pos1);
+				ft_putchar(pos2);
+				ft_putchar(pos3);
+				if (!(pos1 == '7' && pos2 == '8' && pos3 == '9'))
+					ft_putchar(',');
+				ft_putchar(' ');
+				pos3++;
+			}
+			pos2++;
+		}
+		pos1++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
